826dfdd55f853e71698d5c3e9c50baf4c6dbb3c5
DevDejvid <80787278+DevDejvid@users.noreply.github.com>
2021-03-23 15:45:39 +0000

    Use full unique user IDs

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ client.on('message', (message) => {
     // ignoruj správy od botov a adminov
     if (!message.author.bot && !message.member.hasPermission('ADMINISTRATOR')) {
         // prečítaj obsah spravy a skontroluj, či autor bol oboznamený
-        msganalyzer.read(message.content, message.author.tag.split('#')[1])
+        msganalyzer.read(message.content, message.author.id)
         .then((response) => {
             // ak sa v správe nachádza keyword, odpíš autorovi s odkazom na potenciálnu odpoveď k dotazu
             if (response) {
