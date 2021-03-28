e444e64aaf40f090288c39ac87e48285e0df9532
Vojtch159 <vojta159.studeny@gmail.com>
2021-03-28 22:34:34 +0200

    Ignore roles update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as discord from 'discord.js';
 import * as dotenv from 'dotenv';
 import * as msganalyzer from './message_analyzer';
+import * as config from '../config/config';
 
 const client = new discord.Client();
 
@@ -12,17 +13,33 @@ client.once('ready', () => {
 });
 
 client.on('message', (message) => {
-    // ignoruj správy od botov a adminov
-    if (!message.author.bot || !message.member.hasRole("Admin" || "Moderátor" || "Expert")) {
-        // prečítaj obsah spravy a skontroluj, či autor bol oboznamený
-        msganalyzer.read(message.content, message.author.id)
-        .then((response) => {
-            // ak sa v správe nachádza keyword, odpíš autorovi s odkazom na potenciálnu odpoveď k dotazu
-            if (response) {
-                message.channel.send('Ahoj ' + message.author.toString() + ', ' + response + '.');
-            }
-        });
+    // only filter messages in specified channels
+    if (!config.channelIDs.length || config.channelIDs.includes(message.channel.id)) {
+        // ignore messages from bots, admins and members with specified roles
+        if (!message.author.bot && !message.member.hasPermission('ADMINISTRATOR') && !blacklisted(message.member.roles.cache.array())) {
+            // read message content
+            msganalyzer.read(message.content, message.author.id)
+            .then((response) => {
+                // if message contains keyword and user was not previously notified, reply to user with given response
+                if (response) {
+                    message.channel.send('Ahoj ' + message.author.toString() + ', ' + response + '.');
+                }
+            });
+        }
     }
 });
+
+function blacklisted(roles: discord.Role[]): boolean {
+    // check if user has one of the roles specified in the config file
+    for (let i = 0; i < config.roleIDs.length; i++) {
+        for (let j = 0; j < roles.length; j++) {
+            if (roles[j].id == config.roleIDs[i]) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
 
 client.login(process.env.BOT_TOKEN);
